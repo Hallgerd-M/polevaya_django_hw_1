32c00356fdfe7a02e1b1bf279017fa67d45cb96b
Irina Polevaya <mirwen@yandex.ru>
2024-11-04 04:02:14 +0300

    homework 26.1 finished

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductTemplateView, ProductDetailView
+from catalog.views import (ProductDetailView, ProductListView,
+                           ProductTemplateView)
+
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("contacts/", ProductTemplateView.as_view(), name="contacts"),
     path("catalog/", ProductListView.as_view(), name="catalog"),
-    path("product/<int:pk>", ProductDetailView.as_view(), name="product_detail")
+    path("product/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView, TemplateView
+
 from catalog.models import Product
-from django.views.generic import ListView, DetailView, TemplateView
 
 
 class ProductTemplateView(TemplateView):
